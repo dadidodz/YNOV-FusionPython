bed2304df1d59bde2044f0ffff03f30705994726
Zephyr <nils.jaudon@ynov.com>
2024-04-11 16:46:18 +0200

    chat

--- clientUDP.py
+++ clientUDP.py
@@ -1,19 +1,11 @@
-# -*- coding: utf-8 -*-
-
 import socket
 
-# Paramètres du serveur
-     # Port du serveur
-
-def hostScan():
-    SERVER_HOST = '10.34.0.248'  # Adresse IP du serveur
+def hostScan(pseudo):
+    SERVER_HOST = '10.34.0.248'
     SERVER_PORT = 12345   
-    message = "Bonjour, serveur !"
+    message = f"Pseudo: {pseudo}"
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.sendto(message.encode(), (SERVER_HOST, SERVER_PORT))
     response, _ = client_socket.recvfrom(1024)
     print("Réponse du serveur:", response.decode())
     client_socket.close()
-
-   
-# 
