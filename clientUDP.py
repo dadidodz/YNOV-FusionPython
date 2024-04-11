f3a2f6fc09adc564b8968e182596c6561a758f79
dadidodz <dorian.blatiere@hotmail.fr>
2024-04-11 15:39:54 +0200

    Start

---
+++ clientUDP.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+# Paramètres du serveur
+SERVER_HOST = '127.0.0.1'  # Adresse IP du serveur
+SERVER_PORT = 12345        # Port du serveur
+
+# Message à envoyer
+message = "Bonjour, serveur !"
+
+# Création d'un socket UDP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Envoi du message au serveur
+client_socket.sendto(message.encode(), (SERVER_HOST, SERVER_PORT))
+
+# Recevoir la réponse du serveur
+response, _ = client_socket.recvfrom(1024)
+print("Réponse du serveur:", response.decode())
+
+# Fermer le socket client
+client_socket.close()
---
+++ serveurUDP.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+# Paramètres du serveur
+HOST = '127.0.0.1'  # Adresse IP du serveur
+PORT = 12345        # Port sur lequel le serveur écoute
+
+# Création d'un socket UDP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Lier le socket au port et à l'adresse IP
+server_socket.bind((HOST, PORT))
+
+print("Serveur UDP en attente de messages...")
+
+while True:
+    # Recevoir les données du client
+    data, addr = server_socket.recvfrom(1024)
+    print("Message reçu du client:", data.decode())
+
+    # Répondre au client
+    server_socket.sendto("Message reçu par le serveur.".encode('utf-8'), addr)
+
